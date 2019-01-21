04429e150e653863853f4a6d1d366461ed545e20
bookfairy <libronia1415@gmail.com>
2019-01-21 15:31:27 +0900

    03강: URLConf and Regular Expression

---
+++ dojo/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def mysum(request, numbers):
+    result = sum(list(map(lambda s: int(s or 0), numbers.split("/"))))
+    return HttpResponse(result)
+
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name, age))
