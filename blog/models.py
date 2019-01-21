32803917b686e50d9d6f29643aa69ae52ca5d045
bookfairy <libronia1415@gmail.com>
2019-01-21 17:20:29 +0900

    05강: Model and Model Fields

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+import re
 from django.db import models
+from django.forms import ValidationError
+from django.utils import timezone
 
-# Create your models here.
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError("Invalid LngLat Type")
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력하세요. 최대 100자.')
+    content = models.TextField(verbose_name='내용', help_text='마크다운 문법을 사용해주세요.')
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='경도, 윈도 포맷으로 입력하세요.')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='내용')
+    updated_at = models.DateTimeField(auto_now=True)
