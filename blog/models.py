31a99343f5dc1266bfe409880f83f9c8c788bf34
sudosubin <sudosubin+github@gmail.com>
2019-01-21 18:01:49 +0900

    06강: Migration

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력하세요. 최대 100자.')
     content = models.TextField(verbose_name='내용', help_text='마크다운 문법을 사용해주세요.')
     tags = models.CharField(max_length=100, blank=True)
