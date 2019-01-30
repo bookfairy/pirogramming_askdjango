b2feceab1ada0d30df993dc8d22e6f3f072656f5
bookfairy <libronia1415@gmail.com>
2019-01-30 14:31:02 +0900

    11강: Model Relationship Fields

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 import re
+
+from django.conf import settings
 from django.db import models
 from django.forms import ValidationError
 from django.utils import timezone
@@ -15,13 +17,13 @@ class Post(models.Model):
         ('p', 'Published'),
         ('w', 'Withdrawn'),
     )
-
-    author = models.CharField(max_length=20)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='blog_post_set')
+    # author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력하세요. 최대 100자.')
     content = models.TextField(verbose_name='내용', help_text='마크다운 문법을 사용해주세요.')
-    tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='경도, 윈도 포맷으로 입력하세요.')
     status = models.CharField(max_length=1, choices=STATUC_CHOICE)
+    tag_set = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='내용')
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -30,3 +32,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.CharField(max_length=20)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
