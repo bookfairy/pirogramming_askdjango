6478dc62441d2865e43fa37ffc387d6e4de79860
sudosubin <sudosubin+github@gmail.com>
2019-01-30 14:51:42 +0900

    12강: 장고 템플릿 상속

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -59,7 +59,10 @@ ROOT_URLCONF = 'askdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'askdjango', 'templates'),
+        ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
