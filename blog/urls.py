07776e2d7190b07d4a9e89eb561da238df0a5fc4
bookfairy <libronia1415@gmail.com>
2019-01-30 15:53:49 +0900

    14강: URL Reverse

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 # blog/urls.py
 from django.conf.urls import url
+from blog import views_cbv
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^(?P<id>\d+)/$', views.post_detail),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^detail/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^cbv/new/$', views_cbv.post_new),
 ]
