2b5681a367f2e503968b60a31c007f7ed12cc744
bookfairy <libronia1415@gmail.com>
2019-02-01 15:37:53 +0900

    25강: Message Framework - 임시 메세지 활용하기

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -126,3 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+from django.contrib.messages import constants
+MESSAGE_LEVEL = constants.DEBUG
+MESSAGE_TAGS = {constants.ERROR: 'DANGER'}
