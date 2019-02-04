6b9b555650ae6229e17fcf37b61339955f58da81
sudosubin <sudosubin+github@gmail.com>
2019-02-05 01:39:49 +0900

    29강: ImageField 썸네일(Feat. django-imagekit)

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 import re
-
 from django.conf import settings
 from django.db import models
 from django.forms import ValidationError
 from django.urls import reverse
 from django.utils import timezone
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
 
 
 def lnglat_validator(value):
@@ -22,7 +23,8 @@ class Post(models.Model):
     # author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력하세요. 최대 100자.')
     content = models.TextField(verbose_name='내용', help_text='마크다운 문법을 사용해주세요.')
-    photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d')
+    photo = ProcessedImageField(blank=True, upload_to='blog/post/%Y/%m/%d', processors=[Thumbnail(300, 300)],
+                                format='JPEG', options={'quality': 60})
     lnglat = models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='경도, 윈도 포맷으로 입력하세요.')
     status = models.CharField(max_length=1, choices=STATUC_CHOICE)
     tag_set = models.ManyToManyField('Tag', blank=True)
