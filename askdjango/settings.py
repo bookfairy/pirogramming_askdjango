73293ce9ed5aacde0c5395cc860a52d99a3233b6
bookfairy <libronia1415@gmail.com>
2019-02-04 23:03:25 +0900

    26강: Static Files

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -124,9 +124,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askdjango', 'static'),
+    # os.path.join(BASE_DIR, 'static'),
+]
 
 INTERNAL_IPS = ['127.0.0.1']
 
 from django.contrib.messages import constants
+
 MESSAGE_LEVEL = constants.DEBUG
 MESSAGE_TAGS = {constants.ERROR: 'DANGER'}
