cb139c9dec9926a8f91b5593b84b28ef3a5219ea
sudosubin <sudosubin+github@gmail.com>
2019-02-04 23:48:38 +0900

    27강: Media Files

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -131,6 +131,9 @@ STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 INTERNAL_IPS = ['127.0.0.1']
 
 from django.contrib.messages import constants
