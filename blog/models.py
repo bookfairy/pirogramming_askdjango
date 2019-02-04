d1bc3979d8d7ddbb3e535ab1420f0fc2426a9b2b
bookfairy <libronia1415@gmail.com>
2019-02-04 23:48:38 +0900

    27강: Media Files

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     # author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력하세요. 최대 100자.')
     content = models.TextField(verbose_name='내용', help_text='마크다운 문법을 사용해주세요.')
+    photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d')
     lnglat = models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='경도, 윈도 포맷으로 입력하세요.')
     status = models.CharField(max_length=1, choices=STATUC_CHOICE)
     tag_set = models.ManyToManyField('Tag', blank=True)
