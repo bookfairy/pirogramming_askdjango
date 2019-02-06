183c2c6a35efa63ed0a7e7ccdb52d28508ed666a
bookfairy <libronia1415@gmail.com>
2019-02-06 17:06:08 +0900

    31강: 클래스 기반 뷰 (Class Based View) 소개 6

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,11 @@ from blog import views_cbv
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^detail/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^$', views_cbv.post_list, name='post_list'),
+    url(r'^detail/(?P<pk>\d+)/$', views_cbv.post_detail, name='post_detail'),
     url(r'^new/$', views.post_new, name='post_new'),
     url(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^cbv/new/$', views_cbv.post_new),
+    url(r'^cbv/(?P<pk>\d+)/edit/$', views_cbv.post_edit),
+    url(r'^cbv/(?P<pk>\d+)/delete/$', views_cbv.post_delete),
 ]
